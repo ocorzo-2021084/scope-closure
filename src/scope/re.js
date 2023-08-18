dd79e1e317c9370ab8df59df60ab980df24dd212
Octavio Corzo <ocorzo-2021084@kinal.edu.gt>
2023-08-18 09:44:16 -0600

    Strict mode

---
+++ src/scope/re.js
@@ -0,0 +1,19 @@
+var firstName; 
+firstName = 'Octavio'
+console.log(firstName);
+
+var lastName = "Alejandro"; //Declarar y asignar
+lastName = "David"; // reasignar
+console.log(lastName);
+
+var secondName = 'pepe';
+var secondName = 'Ana';
+console.log(secondName);
+
+//let
+let fruta = 'Manzana';
+fruta = 'Fresa';
+
+//const
+const animal = 'Perro';
+animal = 'gato';
